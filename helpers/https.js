14e6cda5fa63d0d66bbfa5307f6f3d4a86ba5990
Daniel <danielsalles@gmail.com>
2017-07-11 03:38:44 -0300

    Mini refactor https

--- helpers/https.js
+++ helpers/https.js
@@ -3,16 +3,14 @@
 const axios = require('axios')
 const daysValidate = require('./validate').days
 
-const makeAxios = opt => {
-	return axios.create({
-		baseURL: 'https://www.receitaws.com.br/v1/cnpj/',
-		timeout: opt.timeout,
-		headers: hasToken(opt.token) ? {'Authorization:': opt.token} : null
-	})
-}
+const makeAxios = opt => axios.create({
+	baseURL: 'https://www.receitaws.com.br/v1/cnpj/',
+	timeout: opt.timeout,
+	headers: hasToken(opt.token) ? {'Authorization:': opt.token} : null
+})
 
 const hasToken = token => token !== null
 
 module.exports = (cnpj, days, opt) => makeAxios(opt)
-									.get(hasToken(opt.token) ? `/${cnpj}/days/${opt.days}` : `/${cnpj}`)
-									.then(v => v.data)
+.get(hasToken(opt.token) ? `/${cnpj}/days/${opt.days}` : `/${cnpj}`)
+.then(v => v.data)
