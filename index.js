2f425d88603f87b70faedb72da66e084601c731a
Daniel <danielsalles@gmail.com>
2017-07-14 02:59:21 -0300

    Remove tests

--- index.js
+++ index.js
@@ -5,15 +5,3 @@ const defineDefault = require('./helpers/utils').defineDefault
 const validateTypes = R.pipe(defineDefault, validateOpt)
 
 const receitaWS = options => (cnpj, days = null) => https(cnpj, days, validateTypes(options))
-
-// TESTE BOBO
-// ARGUMENTO OPT
-// timeout, token
-
-// Caso tenha token ele vá usar a premium
-const opt = {
-	timeout: 5000
-}
-const instance = receitaWS(opt)
-instance('27865757000102')
-	.then(v => console.log(v))
