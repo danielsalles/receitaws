9c0e7c4c401b2b12d8e1a9909357dbb659463bd8
Daniel <danielsalles@gmail.com>
2017-07-14 02:58:44 -0300

    Add new test

--- helpers/validate.js
+++ helpers/validate.js
@@ -2,8 +2,11 @@ const isBoolean = require('lodash').isBoolean
 const isString = require('lodash').isString
 const isNull = require('lodash').isNull
 const isInteger = require('lodash').isInteger
+const isObjectLike = require('lodash').isObjectLike
 
 exports.options = obj => {
+	if(obj === undefined) throw new Error('É obrigatório passar um parâmetro')
+	if(isObjectLike(obj) === false ) throw new Error('O argumento precisa ser um objeto valido.')
 	if(isInteger(obj.timeout) === false) throw new Error('O elemento timeout precisa ser Integer.')
 	if((isString(obj.token) || isNull(obj.token)) === false) throw new Error('O elemento token precisa ser string ou null.')
 
