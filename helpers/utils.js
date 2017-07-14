6e4da26844fe01b73cd93893532b965373c36234
Daniel <danielsalles@gmail.com>
2017-07-14 14:47:42 -0300

    Ajust Default values

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,7 +1,8 @@
 const isUndefined = require('lodash').isUndefined
 
 exports.defineDefault = opt => {
-	if(isUndefined(opt.timeout)) opt.timeout = 5000
+	if(isUndefined(opt)) opt = {}
+	if(isUndefined(opt.timeout)) opt.timeout = 10000
 	if(isUndefined(opt.token)) opt.token = null
 
 	return opt
